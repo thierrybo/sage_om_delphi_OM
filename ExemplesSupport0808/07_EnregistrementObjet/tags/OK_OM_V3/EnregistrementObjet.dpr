3ad1271a33d7c350c450fcc2e6d51d81041b07ee
tbothorel <tbothorel@mbg.fr>
2011-07-30 13:07:55 +0000

    git-svn-id: https://po134.groupeadinfo.local/svn/OM@47 6e882e4b-c362-7340-80c8-0e5588b42b4b

---
+++ ExemplesSupport0808/07_EnregistrementObjet/tags/OK_OM_V3/EnregistrementObjet.dpr
@@ -0,0 +1,58 @@
+{******************************************************************************}
+{                                                                              }
+{       Copyright (c) 2009 MBG partenaires                                     }
+{                                                                              }
+{******************************************************************************}
+
+program EnregistrementObjet;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  Commun in '..\..\Commun.pas',
+  Objets100Lib_TLB,
+  ActiveX;
+
+var
+  StreamCpta : TBSCPTAApplication3;
+  BaseCpta   : IBSCPTAApplication3;
+  Client     : IBOClient3;
+
+begin
+  CoInitialize(nil);
+  StreamCpta := TBSCPTAApplication3.Create(nil);
+  BaseCpta   := StreamCpta.DefaultInterface;
+  try
+    if OuvreBaseCpta(BaseCpta,
+      'C:\Documents and Settings\All Users\Documents\Sage\Sage Entreprise\BIJOU.MAE'
+      ) then
+    begin
+      try
+        Client := BaseCpta.FactoryClient.Create as IBOClient3;
+        With Client do
+        begin
+          CT_Num       := 'BOLLE';
+          CT_Intitule  := 'Bolle Virginie';
+          TiersPayeur  := Client;
+          CompteGPrinc := BaseCpta.FactoryCompteG.ReadNumero('4110000');
+          Write;
+        end;
+        Writeln(StrToOem('Client correctement créé'));
+      except
+        on E:Exception do
+          Writeln(
+            StrToOem('Erreur en création du client : '),
+            sLineBreak,
+            E.Classname,
+            ': ',
+            StrToOem(E.Message));
+      end;
+    end;
+  finally
+    FermeBaseCpta(BaseCpta);
+    FreeAndNil(StreamCpta);
+    CoUnInitialize;
+    Readln;
+   end;
+end.
