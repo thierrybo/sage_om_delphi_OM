7bfead43133156807ac6312ab4d82f8ad0d0cfb3
tbothorel <tbothorel@mbg.fr>
2011-07-30 13:08:57 +0000

    OK OM V3
    git-svn-id: https://po134.groupeadinfo.local/svn/OM@49 6e882e4b-c362-7340-80c8-0e5588b42b4b

---
+++ ExemplesSupport0808/08_SuppressionTiers/trunk/SuppressionTiers.dpr
@@ -0,0 +1,65 @@
+{******************************************************************************}
+{                                                                              }
+{       Copyright (c) 2009 MBG partenaires                                     }
+{                                                                              }
+{******************************************************************************}
+
+program SuppressionTiers;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  Commun in '..\..\Commun.pas',
+  Objets100Lib_TLB,
+  ActiveX;
+
+var
+  StreamCpta : TBSCPTAApplication3;
+  BaseCpta   : IBSCPTAApplication3;
+
+function SupprimeTiers(var ABaseCpta: IBSCPTAApplication3; ANumTiers: string):
+  Boolean;
+var
+  ObjetTiers : IBOTiers3;
+begin
+  try
+    ObjetTiers  := ABaseCpta.FactoryTiers.ReadNumero(ANumTiers);
+    ObjetTiers.Remove;
+    Result      := true;
+  except on E: Exception do
+    begin
+    Writeln(
+      'Erreur en suppression de l''enregistrement ',
+      ANumTiers,
+      ' dans la table F_COMPTET : ',
+      sLineBreak,
+      E.ClassName,
+      ': ',
+      StrToOem(E.Message));
+    Result := false;
+    end;
+  end;
+end;
+
+begin
+  CoInitialize(nil);
+  StreamCpta := TBSCPTAApplication3.Create(nil);
+  BaseCpta   := StreamCpta.DefaultInterface;
+  try
+    if OuvreBaseCpta(BaseCpta,
+      'C:\Documents and Settings\All Users\Documents\Sage\Sage Entreprise\BIJOU.MAE'
+      ) then
+    begin
+      if SupprimeTiers(BaseCpta, 'ZAN') then
+        Writeln(StrToOem('Tiers ZAN supprimé !'));
+      if SupprimeTiers(BaseCpta, 'CARAT') then
+        Writeln(StrToOem('Tiers CARAT supprimé !'));
+    end;
+  finally
+    FermeBaseCpta(BaseCpta);
+    FreeAndNil(StreamCpta);
+    CoUnInitialize;
+    Readln;
+   end;
+end.
